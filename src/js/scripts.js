bb300a68131c19e5a012d4c3d55ad7d3fc687ed0
Vitalii <nailbomb12@gmail.com>
2018-07-11 19:47:18 +0300

    some fix

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,6 +3,7 @@ const stub = document.querySelector('.stub');
 const aside = document.querySelector('.aside');
 const asideList = document.querySelector('.aside__list');
 const asideItem = document.querySelectorAll('.aside__item')
+const asideLink = document.querySelectorAll('.aside__link')
 const tabs = document.querySelector('.category-list');
 const tabLinks = document.querySelectorAll('.category-item');
 const tabsPane = document.querySelectorAll('.tabs__pane');
@@ -10,9 +11,11 @@ const serials = document.querySelector('.tv-serials');
 const hiddenBlockIcon = document.querySelector('.hidden-search');
 const hiddenBlock = document.querySelector('.hidden');
 
-tabLinks[0].classList.add('tabs__link--active');
+tabLinks[0].classList.add('category-item--active');
 tabsPane[0].classList.add('tabs__pane--active');
 
+
+/**при нажатии на бургер показывает меню */
 const toggleAside = () => {
   aside.classList.toggle('js-show-aside');
   stub.classList.add('js-show-stub')
@@ -20,14 +23,7 @@ const toggleAside = () => {
 
 menu.addEventListener('click', toggleAside);
 
-const toggleCategorys = (evt) => {
-  if (evt.target.classList.contains('aside__item')){
-    evt.target.classList.toggle('aside__item-active');
-  }
-}
-
-asideList.addEventListener('click', toggleCategorys);
-
+/**при нахатии на поиск опускается штора */
 const toggleHiddenBlock = () => {
   hiddenBlock.classList.toggle('js-show-hidden');
   stub.classList.add('js-show-stub')
@@ -35,6 +31,8 @@ const toggleHiddenBlock = () => {
 
 hiddenBlockIcon.addEventListener('click', toggleHiddenBlock);
 
+
+/**для скрытия всех плавающих блоков */
 const hideBlocks = (evt) => {
   if (stub.classList.contains('js-show-stub')){
     hiddenBlock.classList.remove('js-show-hidden');
@@ -45,6 +43,9 @@ const hideBlocks = (evt) => {
 
 stub.addEventListener('click', hideBlocks);
 
+
+
+/** */
 function categorySwitcher() {
   const categories = document.querySelector('.category-list');
   const categoryItems = document.querySelectorAll('.category-item'); 
@@ -64,7 +65,8 @@ function categorySwitcher() {
   }
 }
 
-const searchBtn = document.querySelector('.idBtn')
+const searchBtn = document.querySelector('.idBtn');
+
 const onClickHandler = (event) => {
   event.preventDefault(0);
   if (event.target.classList.contains('idBtn')) {
@@ -73,19 +75,23 @@ const onClickHandler = (event) => {
   if (idInput.value == '') return;
 };
 
-// const onCetegoryClick = (event) => {
-//   if (event.target.tagName.id ='category-serial') {
-//     getPopularTV();
-//   }
-// }
+const onCetegoryClick = (event) => {
+  if (event.target.tagName.id ='category-serial') {
+    getPopularTV();
+  }
+}
 
-// Category switcher function
+Category switcher function
 
-const onClickHandlers = (event) => {
+
+
+
+
+const renderCardsTemplate = (event) => {
   event.preventDefault();
   if (event.target !== tabs) {
-    tabLinks.forEach(link => link.classList.remove('tabs__link--active'));
-    event.target.classList.add('tabs__link--active');
+    tabLinks.forEach(link => link.classList.remove('category-item--active'));
+    event.target.classList.add('category-item--active');
     tabsPane.forEach(tabs => tabs.classList.remove('tabs__pane--active'));
   for (let tab of tabsPane) {
     if (event.target.getAttribute('href') === ('#' + tab.id))
@@ -94,10 +100,38 @@ const onClickHandlers = (event) => {
   }
 }
 document.addEventListener("DOMContentLoaded", getPopularTV());
-tabs.addEventListener('click', onClickHandlers);
-searchBtn.addEventListener('click', onClickHandler);
+tabs.addEventListener('click', renderCardsTemplate);
+searchBtn.addEventListener('click', renderCardsTemplate);
 
 
-//category.addEventListener('click', onCetegoryClick);
+category.addEventListener('click', onCetegoryClick);
 
-//categorySwitcher();
+categorySwitcher();
+
+
+
+
+/**Функция для переключения категорий в эсайде */
+const a = (evt) => {
+  if (evt.target.classList.contains('aside__link')){
+    asideLink.forEach(link => {
+      link.classList.remove('aside__item-active')
+    });
+    evt.target.classList.toggle('aside__item-active');
+    tabsPane.forEach(tabs => tabs.classList.remove('tabs__pane--active'));
+    for (let tab of tabsPane) {
+      if (evt.target.getAttribute('href') === ('#' + tab.id))
+        tab.classList.add('tabs__pane--active');
+    }  
+
+  }
+}
+asideList.addEventListener('click', a);
+
+// const toggleCategorys = (evt) => {
+//   if (evt.target.classList.contains('aside__item')){
+//     evt.target.classList.toggle('aside__item-active');
+//   }
+// }
+
+// asideList.addEventListener('click', toggleCategorys);
